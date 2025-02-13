9fc4b39aca767523d836f0100724e0fce14ad648
Subodh Adhikari <subodhadhikari2023@outlook.com>
2025-02-13 12:43:39 +0530

    Added asynchronous mailing for reduction of mail time

---
+++ 00-Backend/InternshipPortal/src/main/java/com/subodh/InternshipPortal/entities/RegistrationResponse.java
@@ -0,0 +1,14 @@
+package com.subodh.InternshipPortal.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RegistrationResponse {
+    private String response;
+
+}
--- 00-Backend/InternshipPortal/src/main/java/com/subodh/InternshipPortal/services/Implementation/MailServiceImpl.java
+++ 00-Backend/InternshipPortal/src/main/java/com/subodh/InternshipPortal/services/Implementation/MailServiceImpl.java
@@ -4,6 +4,7 @@ import com.subodh.InternshipPortal.services.MailService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ public class MailServiceImpl implements MailService {
 
     }
 
+    @Async("taskExecutor")
     @Override
     public void sendMail(String toEmail, String subject, String body) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
