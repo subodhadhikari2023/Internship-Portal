16eeea8bb99538e8c82e49210b338f05f61167fc
Subodh Adhikari <subodhadhikari2023@outlook.com>
2025-02-19 11:50:37 +0530

    Updated the instructor home page component-> added navbar, Updated the landing component-> Added responsive navbar and updated the image gallery

--- front-end/src/app/components/landing/landing.component.ts
+++ front-end/src/app/components/landing/landing.component.ts
@@ -9,33 +9,18 @@ export class LandingComponent implements OnInit {
 
   constructor() { }
 
-
   bannerImageUrls = [
     "assets/images/main/buddha.jpg",
-    "assets/images/main/dham.jpg",
+    // "assets/images/main/dham.jpg",
     "assets/images/main/house.jpg",
     "assets/images/main/mountaintop.jpg",
     "assets/images/main/peaks.jpg",
     "assets/images/main/river.jpg",
     "assets/images/main/waterfall.jpg",
     "assets/images/main/whitepeaks.jpg",
- 
+
 
   ];
-  heroTexts = [
-    'Welcome to Sikkim\'s Internship Portal!',
-    'Explore New Opportunities in the Himalayas!',
-    'Join Us for an Unforgettable Experience!',
-    'Discover the Tranquility of Buddha\'s Teachings!',
-    'Experience the Rich Culture of Dham!',
-    'Enjoy the Serenity of Traditional Sikkimese Houses!',
-    'Reach New Heights at Our Mountain Tops!',
-    'Witness the Majestic Peaks of Sikkim!',
-    'Immerse Yourself in the Beauty of Rivers and Waterfalls!'
-  ];
-  
-
-
   currentImageIndex = 0;
 
 
@@ -46,9 +31,18 @@ export class LandingComponent implements OnInit {
 
   }
 
-    isMenuOpen = false;
-  
+  isMenuOpen = false;
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
+
+  isNavbarOpen = false;
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
+
 }
+
