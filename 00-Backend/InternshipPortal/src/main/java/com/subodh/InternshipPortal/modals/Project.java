133e40aea4e6ddd31a45af0840ea46a1cab34432
Subodh Adhikari <subodhadhikari2023@outlook.com>
2025-03-11 23:40:23 +0530

    Added project entity and it's service classes

---
+++ 00-Backend/InternshipPortal/src/main/java/com/subodh/InternshipPortal/modals/Project.java
@@ -0,0 +1,44 @@
+package com.subodh.InternshipPortal.modals;
+
+import com.subodh.InternshipPortal.enums.StudentInternshipStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long projectId;
+
+    @Column(nullable = false)
+    private String projectName;
+
+    @Column(nullable = false)
+    private String projectDescription;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    private String fileType;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "student_id", referencedColumnName = "user_id")
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "internship_id", referencedColumnName = "internship_id")
+    private Internship internship;
+
+    @Column(nullable = false)
+    private LocalDate uploadDate;
+
+    @Enumerated(EnumType.STRING)
+    private StudentInternshipStatus status;
+}
