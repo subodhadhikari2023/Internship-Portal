19f10a0d588fd5754bb021ba6ebf881bcaa05e2a
Subodh Adhikari <subodhadhikari2023@outlook.com>
2025-03-23 13:02:06 +0530

    Added the default profile picture instead of text in the update profile container

--- front-end/src/app/components/update-profile-student/update-profile-student.component.ts
+++ front-end/src/app/components/update-profile-student/update-profile-student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -11,40 +12,41 @@ export class UpdateProfileStudentComponent implements OnInit {
   studentData: any = {};
   isEditing: boolean = false;
 
-  // constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    // this.fetchStudentDetails();
+    this.fetchStudentDetails();
   }
 
-  // fetchStudentDetails() {
-  //   this.userService.fetchStudentDetails().subscribe({
-  //     next: (res) => {
-  //       this.studentData = res;
-  //     },
-  //     error: (err) => {
-  //       console.error(err);
-  //     }
-  //   });
-  // }
+  fetchStudentDetails() {
+    this.userService.fetchStudentDetails().subscribe({
+      next: (res) => {
+        // console.log(res);        
+        this.studentData = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 
-  // enableEditing() {
-  //   this.isEditing = true;
-  // }
+  enableEditing() {
+    this.isEditing = true;
+  }
 
-  // cancelEditing() {
-  //   this.isEditing = false;
-  //   this.fetchStudentDetails(); // Reset changes if canceled
-  // }
+  cancelEditing() {
+    this.isEditing = false;
+    this.fetchStudentDetails(); // Reset changes if canceled
+  }
 
-  // updateProfile() {
-  //   this.userService.updateStudentProfile(this.studentData).subscribe({
-  //     next: (res) => {
-  //       this.isEditing = false;
-  //     },
-  //     error: (err) => {
-  //       console.error(err);
-  //     }
-  //   });
-  // }
+  updateProfile() {
+    this.userService.updateStudentProfile(this.studentData).subscribe({
+      next: (res) => {
+        this.isEditing = false;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
